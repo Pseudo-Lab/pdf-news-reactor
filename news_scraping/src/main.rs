c27348f42f9f29e82166b91f9e29dad0b67f72b7
4roring <4roring@naver.com>
2024-09-22 17:51:30 +0900

    ignore 수정, main 테스트 작업중인 부분 커밋

--- news_scraping/src/main.rs
+++ news_scraping/src/main.rs
@@ -1,3 +1,19 @@
+use reqwest;
+use reqwest::blocking::Client;
+use scraper;
+use select::document::Document;
+
+fn scraping(url: &str) {
+    let client = Client::new();
+    let response = client.get(url).send();
+
+    let data = response.unwrap().text().unwrap();
+    println!("{data}");
+
+    let html_product_selector = scraper::Selector::parse("div.col").unwrap();
+    let html_products = document.select(&html_product_selector);
+}
+
 fn main() {
-    println!("Hello, world!");
+    scraping("https://demo.opencart.com/")
 }
