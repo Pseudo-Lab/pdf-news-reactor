f35ba00d863d2bcee9fb47c21722d090799eb593
ehddnr301 <dy950328@gmail.com>
2024-11-03 07:55:52 +0000

    Update table

--- README.md
+++ README.md
@@ -10,7 +10,10 @@
 ## Clickhouse
 
 - 쿼리해보기
-```
+```sql
 select * from reaction_table
 settings stream_like_engine_allow_direct_select = 1
+```
+```sql
+select * from local_table
 ```
--- crawling/push_article_id.py
+++ crawling/push_article_id.py
@@ -6,14 +6,18 @@ TASK_LIST_KEY = "task_list"
 redis_client = redis.Redis(host="localhost", port=30007, db=0)
 
 
-redis_client.rpush(TASK_LIST_KEY, "0000283914")
+# redis_client.rpush(TASK_LIST_KEY, "0000283914")
 
-print(redis_client.lrange(TASK_LIST_KEY, 0, -1))
+# print(redis_client.lrange(TASK_LIST_KEY, 0, -1))
 
 # redis_client.flushall()
 
 import time
 
-time.sleep(10)
 
-redis_client.rpush(TASK_LIST_KEY, "0000283915")
+# redis_client.rpush(TASK_LIST_KEY, "0000283915")
+
+initial = 283914
+for i in range(50):
+    time.sleep(10)
+    redis_client.rpush(TASK_LIST_KEY, f"0000{initial + i}")
