40ee78fa733d135fb52ec71c231bdfff9019ca5b
seongdong <chosd1123@naver.com>
2025-03-03 15:10:16 +0900

    content.js 로직 작성 완료

--- chrome_extension/js/content.js
+++ chrome_extension/js/content.js
@@ -29,9 +29,18 @@
  12. (결과 2) "부족한 데이터로 조회가 어렵습니다." 표시
 **/
 
-const journalistNameElement = document.querySelector('.media_end_head_journalist_name');
-const journalistUrlElement = document.querySelector('div.media_journalistcard_summary_text a:first-child');
+let urlValue = "testurl";
+let usefulcount = 9999;
+let wowcount = 9999;
+let touchedcount = 9999;
+let analyticalcount = 9999;
+let recommendcount = 9999;
 
+const journalistNameElement = document.querySelector('.media_end_head_journalist_name');
+//const journalistUrlElement = document.querySelector('div.media_journalistcard_summary_text a:first-child');
+const journalistUrlElement = document.querySelector('a.media_journalistcard_summary_photo');
+urlValue = journalistUrlElement.getAttribute('href');
+console.log(urlValue);
 
 if (journalistNameElement) {
   const journalistName = journalistNameElement.innerText.trim();
@@ -41,16 +50,23 @@ if (journalistNameElement) {
 }
 
 function update() {
-  const likeCountElement = document.querySelectorAll('div._reactionModule.u_likeit.nv_notrans span.u_likeit_list_count._count');
-  const usefulcount = parseInt(likeCountElement[0].textContent) || 0;
-  const wowcount = parseInt(likeCountElement[1].textContent) || 0;
-  const touchedcount = parseInt(likeCountElement[2].textContent) || 0;
-  const analyticalcount = parseInt(likeCountElement[3].textContent) || 0;
-  const recommendcount = parseInt(likeCountElement[4].textContent) || 0;
+  let likeCountElement = document.querySelectorAll('div._reactionModule.u_likeit.nv_notrans span.u_likeit_list_count._count');
+  let usefulcount = parseInt(likeCountElement[0].textContent) || 0;
+  let wowcount = parseInt(likeCountElement[1].textContent) || 0;
+  let touchedcount = parseInt(likeCountElement[2].textContent) || 0;
+  let analyticalcount = parseInt(likeCountElement[3].textContent) || 0;
+  let recommendcount = parseInt(likeCountElement[4].textContent) || 0;
+
+  console.log("journalisturl: ", urlValue)
+  console.log("useful: ", usefulcount)
+  console.log("wow: ", wowcount)
+  console.log("touched: ", touchedcount)
+  console.log("analytical: ", analyticalcount)
+  console.log("recommend: ", recommendcount)
 
   chrome.storage.local.set(
                   {
-                      journalisturl: journalistUrlElement,
+                      journalisturl: urlValue,
                       useful: usefulcount,
                       wow: wowcount,
                       touched: touchedcount,
@@ -63,21 +79,24 @@ function update() {
               );
 }
 
-async function validate() {
+function validate() {
   try {
-    const response = await fetch("http://144.24.93.80:8088/api/journalist/validate", {
+    const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/validate", {
       method: "POST",
       body: JSON.stringify({
-        "journalisturl": journalistUrlElement,
+        "journalisturl": urlValue,
       }),
     });
+
+    console.log(urlValue)
+    console.log(response)
 
     if (!response.ok) {
       console.error("HTTP Error:", response.status);
       return false;
     }
 
-    const result = await response.json();
+    const result = response.json();
 
     if (typeof result === 'boolean') {
         console.log("Validation Result (boolean):", result);
@@ -98,17 +117,17 @@ async function validate() {
     return false;
   } catch (error) {
     console.error("Fetch Error:", error);
-    return false; // Return false on network or other errors
+    return false;
   }
 }
 
 
 function add(usefulcount, wowcount, touchedcount, analyticalcount, recommendcount){
     try {
-        const response = fetch("http://144.24.93.80:8088/api/journalist/add-score", {
+        const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/add-score", {
           method: "POST",
           body: JSON.stringify({
-            "journalisturl": journalistUrlElement,
+            "journalisturl": urlValue,
             "useful": usefulcount,
             "touched": touchedcount,
             "recommend": recommendcount,
@@ -145,12 +164,12 @@ function updateLikeCount() {
   add(usefulcount, wowcount, touchedcount, analyticalcount, recommendcount);
 }
 
-async function average() {
+function select() {
     try {
-        const response = await fetch("http://144.24.93.80:8088/api/journalist/average-score", {
+        const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/average-score", {
             method: "POST",
             body: JSON.stringify({
-                "journalisturl": journalistUrlElement,
+                "journalisturl": urlValue,
             }),
         });
 
@@ -158,23 +177,16 @@ async function average() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const result = await response.json();
+        const result = response.json();
         const allZero = Object.values(result).slice(1).every(value => value === 0);
 
         if (allZero) {
             console.log("부족한 데이터로 조회가 어렵습니다.");
             alert("부족한 데이터로 조회가 어렵습니다.");
         } else {
-            const journalisturl = result.journalisturl;
-            const usefulcount = result.useful;
-            const wowcount = result.wow;
-            const touchedcount = result.touched;
-            const analyticalcount = result.analytical;
-            const recommendcount = result.recommend;
-
             chrome.storage.local.set(
                 {
-                    journalisturl: journalisturl,
+                    journalisturl: urlValue,
                     useful: usefulcount,
                     wow: wowcount,
                     touched: touchedcount,
@@ -194,17 +206,38 @@ async function average() {
     }
 }
 
+fetch("https://pdf.pseudolab-devfactory.com/api/journalist/validate", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json", // Content-Type 헤더를 올바르게 설정합니다.
+  },
+  body: JSON.stringify({
+    "journalisturl": "https://media.naver.com/journalist/001/78042",
+  }),
+})
+  .then((response) => {
+    console.log(response);
+    if (response.status === 422) {
+      // 422 오류 처리
+      response.json().then((errorData) => {
+        console.error("422 오류:", errorData);
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Fetch 오류:", error);
+  });
 
-setTimeout(function () {
-    update();
-    validate().then(isValid => {
-        if (isValid) {
-            console.log("Validation successful. Proceeding with updateLikeCount and then select.");
-            updateLikeCount();
-            average();
-        } else {
-            console.log("Validation failed. Proceeding directly to select.");
-            average();
-        }
-    });
-}, 1000);
+//setTimeout(function () {
+//    update();
+//    validate().then(isValid => {
+//        if (isValid) {
+//            console.log("Validation successful. Proceeding with updateLikeCount and then select.");
+//            updateLikeCount();
+//            select();
+//        } else {
+//            console.log("Validation failed. Proceeding directly to select.");
+//            select();
+//        }
+//    });
+//}, 1000);
