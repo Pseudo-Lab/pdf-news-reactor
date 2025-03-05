d0f934b5fa294fb334267d1331bd47ac66e95793
seongdong <chosd1123@naver.com>
2025-03-05 23:18:32 +0900

    구현 완료

--- chrome_extension/js/content.js
+++ chrome_extension/js/content.js
@@ -1,46 +1,13 @@
-/**
- 1. 기자 url parsing
- 2. [api]api 호출로 url가 등록된 데이터인지 검증
- {
-    "journalisturl" : "http://@"
- }
- 2-1. 등록되지 않은 기자면 > 3번 > 4번
- 2-2. 등록된 기자면 > 3번 스킵 > 사번
- 3. (선택) url를 데이터에 삽입
- 4. 기사의 기자 평가 parsing
- 5. 평가가 모두 0이 아니면 > 7번 > 8번
- 6. 평가가 모두 0이면 > 7번 스킵 > 8번
- 7. [api](선택) uri로 평가 추가
-  {
-     "journalisturl" : "http://@",
-     "useful" : @,
-     "touched" : @,
-     "recommend" : @,
-     "analytical" : @,
-     "wow" : @
-  }
- 8. [api]uri로 평가 평균 조회
-   {
-      "journalisturl" : "http://@",
-   }
- 9. 조회된 평균이 모두 0이면 > 12번
- 10. 조회된 평균이 모두 0이 아니면 > 11번
- 11. (결과 1) 평균을 chrome storage로 set
- 12. (결과 2) "부족한 데이터로 조회가 어렵습니다." 표시
-**/
-
 let urlValue = "testurl";
-let usefulcount = 9999;
-let wowcount = 9999;
-let touchedcount = 9999;
-let analyticalcount = 9999;
-let recommendcount = 9999;
+let usefulcount = 0;
+let wowcount = 0;
+let touchedcount = 0;
+let analyticalcount = 0;
+let recommendcount = 0;
 
 const journalistNameElement = document.querySelector('.media_end_head_journalist_name');
-//const journalistUrlElement = document.querySelector('div.media_journalistcard_summary_text a:first-child');
 const journalistUrlElement = document.querySelector('a.media_journalistcard_summary_photo');
 urlValue = journalistUrlElement.getAttribute('href');
-console.log(urlValue);
 
 if (journalistNameElement) {
   const journalistName = journalistNameElement.innerText.trim();
@@ -79,97 +46,15 @@ function update() {
               );
 }
 
-function validate() {
-  try {
-    const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/validate", {
-      method: "POST",
-      body: JSON.stringify({
-        "journalisturl": urlValue,
-      }),
-    });
-
-    console.log(urlValue)
-    console.log(response)
-
-    if (!response.ok) {
-      console.error("HTTP Error:", response.status);
-      return false;
-    }
-
-    const result = response.json();
-
-    if (typeof result === 'boolean') {
-        console.log("Validation Result (boolean):", result);
-        return result;
-    }
-
-    if (typeof result === 'object' && result !== null && 'isValid' in result) {
-      console.log("Validation Result (object):", result.isValid);
-      return result.isValid;
-    }
-
-    if (typeof result === 'object' && result !== null && 'result' in result) {
-      console.log("Validation Result (object):", result.result);
-      return result.result;
-    }
-
-    console.warn("Unexpected API response format:", result);
-    return false;
-  } catch (error) {
-    console.error("Fetch Error:", error);
-    return false;
-  }
-}
-
-
-function add(usefulcount, wowcount, touchedcount, analyticalcount, recommendcount){
+async function getAverage(url) {
     try {
-        const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/add-score", {
-          method: "POST",
-          body: JSON.stringify({
-            "journalisturl": urlValue,
-            "useful": usefulcount,
-            "touched": touchedcount,
-            "recommend": recommendcount,
-            "analytical": analyticalcount,
-            "wow": wowcount
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = response.json();
-        console.log("API Response:", result);
-      } catch (error) {
-        console.error("API Error:", error);
-      };
-}
-
-function updateLikeCount() {
-  const likeCountElement = document.querySelectorAll('div._reactionModule.u_likeit.nv_notrans span.u_likeit_list_count._count');
-  const usefulcount = parseInt(likeCountElement[0].textContent) || 0;
-  const wowcount = parseInt(likeCountElement[1].textContent) || 0;
-  const touchedcount = parseInt(likeCountElement[2].textContent) || 0;
-  const analyticalcount = parseInt(likeCountElement[3].textContent) || 0;
-  const recommendcount = parseInt(likeCountElement[4].textContent) || 0;
-
-  if (usefulcount === 0 && wowcount === 0 && touchedcount === 0 && analyticalcount === 0 && recommendcount === 0) {
-    console.error("All like counts are zero. This is not allowed.");
-    alert("모든 좋아요 수가 0입니다. 다시 시도해주세요.");
-    return;
-  }
-
-  add(usefulcount, wowcount, touchedcount, analyticalcount, recommendcount);
-}
-
-function select() {
-    try {
-        const response = fetch("https://pdf.pseudolab-devfactory.com/api/journalist/average-score", {
+        const response = await fetch("https://pdf.pseudolab-devfactory.com/api/journalist/average-score", {
             method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
             body: JSON.stringify({
-                "journalisturl": urlValue,
+                "journalisturl": url,
             }),
         });
 
@@ -177,8 +62,8 @@ function select() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const result = response.json();
-        const allZero = Object.values(result).slice(1).every(value => value === 0);
+        const result = await response.json();
+        const allZero = await Object.values(result).slice(1).every(value => value === 0);
 
         if (allZero) {
             console.log("부족한 데이터로 조회가 어렵습니다.");
@@ -186,12 +71,12 @@ function select() {
         } else {
             chrome.storage.local.set(
                 {
-                    journalisturl: urlValue,
-                    useful: usefulcount,
-                    wow: wowcount,
-                    touched: touchedcount,
-                    analytical: analyticalcount,
-                    recommend: recommendcount,
+                    journalisturl: result.journalisturl,
+                    useful_avg: result.useful,
+                    wow_avg: result.wow,
+                    touched_avg: result.touched,
+                    analytical_avg: result.analytical,
+                    recommend_avg: result.recommend,
                 },
                 function () {
                     console.log("Data stored in local storage");
@@ -206,37 +91,76 @@ function select() {
     }
 }
 
-fetch("https://pdf.pseudolab-devfactory.com/api/journalist/validate", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json", // Content-Type 헤더를 올바르게 설정합니다.
-  },
-  body: JSON.stringify({
-    "journalisturl": "https://media.naver.com/journalist/001/78042",
-  }),
-})
-  .then((response) => {
-    console.log(response);
-    if (response.status === 422) {
-      response.json().then((errorData) => {
-        console.error("422 오류:", errorData);
-      });
+async function validate(url) {
+    try {
+        const response = await fetch("https://pdf.pseudolab-devfactory.com/api/journalist/validate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "journalisturl": url,
+            }),
+        });
+        const data = await response.json();
+        return data.exists;
+    } catch (error) {
+        console.error("Fetch 오류:", error);
+        return false;
     }
-  })
-  .catch((error) => {
-    console.error("Fetch 오류:", error);
-  });
+}
 
-//setTimeout(function () {
-//    update();
-//    validate().then(isValid => {
-//        if (isValid) {
-//            console.log("Validation successful. Proceeding with updateLikeCount and then select.");
-//            updateLikeCount();
-//            select();
-//        } else {
-//            console.log("Validation failed. Proceeding directly to select.");
-//            select();
-//        }
-//    });
-//}, 1000);
+async function add(url){
+    try {
+        let likeCountElement = document.querySelectorAll('div._reactionModule.u_likeit.nv_notrans span.u_likeit_list_count._count');
+        let usefulcount = parseInt(likeCountElement[0].textContent) || 0;
+        let wowcount = parseInt(likeCountElement[1].textContent) || 0;
+        let touchedcount = parseInt(likeCountElement[2].textContent) || 0;
+        let analyticalcount = parseInt(likeCountElement[3].textContent) || 0;
+        let recommendcount = parseInt(likeCountElement[4].textContent) || 0;
+
+
+        const response = await fetch("https://pdf.pseudolab-devfactory.com/api/journalist/add-score", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "journalisturl": url,
+            "useful": usefulcount,
+            "touched": touchedcount,
+            "recommend": recommendcount,
+            "analytical": analyticalcount,
+            "wow": wowcount
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("API Response:", result);
+      } catch (error) {
+        console.error("API Error:", error);
+      };
+}
+
+setTimeout(async function () {
+    try {
+        update();
+        const isValid = await validate(urlValue);
+        console.log(isValid);
+        if (isValid) {
+            console.log("Validation successful. Proceeding directly to select.");
+            add(urlValue);
+            getAverage(urlValue);
+        } else {
+            console.log("Validation failed. Proceeding with updateLikeCount and then select.");
+            add(urlValue);
+            getAverage(urlValue);
+        }
+    } catch (error) {
+        console.error("setTimeout 오류:", error);
+    }
+}, 1000);
--- chrome_extension/js/popup.js
+++ chrome_extension/js/popup.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     chrome.storage.local.get('journalist', function(result) {
       if (result.journalist) {
         document.getElementById('stats').innerHTML += `
@@ -9,15 +8,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     );
 
-    chrome.storage.local.get( ['useful', 'wow', 'touched', 'analytical', 'recommend'] , function(result) {
+    chrome.storage.local.get( ['useful', 'wow', 'touched', 'analytical', 'recommend','useful_avg', 'wow_avg', 'touched_avg', 'analytical_avg', 'recommend_avg'] , function(result) {
     const journalistStats = {
           'useful': result.useful,
           'touched': result.touched,
           'recommend': result.recommend,
           'analytical': result.analytical,
           'wow': result.wow
-        };
-        const korStats = {
+    };
+    const journalistAverage = {
+          'useful_avg' : result.useful_avg,
+          'touched_avg' : result.touched_avg,
+          'recommend_avg' : result.recommend_avg,
+          'analytical_avg' : result.analytical_avg,
+          'wow_avg' : result.wow_avg
+
+    };
+    const korStats = {
             "touched": "공감백배",
             "warm": "훈훈해요",
             "analytical": "분석탁월",
@@ -27,44 +34,57 @@ document.addEventListener('DOMContentLoaded', function () {
             "recommend": "후속강추",
             "angry": "화나요",
             "useful": "쏠쏠정보",
-            "wow": "흥미진진"
-        }
+            "wow": "흥미진진",
+    }
 
-        const ctx = document.getElementById('radarChart').getContext('2d');
-        const radarChart = new Chart(ctx, {
-            type: 'radar',
-            data: {
-                labels: [korStats.useful , korStats.touched, korStats.recommend, korStats.analytical, korStats.wow],
-                datasets: [{
-                    label: 'Journalist Stats',
-                    data: [
-                        journalistStats.useful,
-                        journalistStats.touched,
-                        journalistStats.recommend,
-                        journalistStats.analytical,
-                        journalistStats.wow
-                    ],
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                }]
+    const ctx = document.getElementById('radarChart').getContext('2d');
+    const radarChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: [korStats.useful , korStats.touched, korStats.recommend, korStats.analytical, korStats.wow],
+            datasets: [{
+                label: 'Journalist Stats',
+                data: [
+                    journalistStats.useful,
+                    journalistStats.touched,
+                    journalistStats.recommend,
+                    journalistStats.analytical,
+                    journalistStats.wow
+                ],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                borderWidth: 1
             },
-            options: {
-                scales: {
-                    r: {
-                        beginAtZero: true
-                    }
+            {
+                label: 'Journalist Average',
+                data: [
+                    journalistAverage.useful_avg,
+                    journalistAverage.touched_avg,
+                    journalistAverage.recommend_avg,
+                    journalistAverage.analytical_avg,
+                    journalistAverage.wow_avg
+                ],
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                r: {
+                    beginAtZero: true
                 }
             }
-        });
-        console.log(radarChart)
-        document.getElementById('stats').innerHTML += `
-              <p>Useful: ${journalistStats.useful}</p>
-              <p>Touched: ${journalistStats.touched}</p>
-              <p>Recommend: ${journalistStats.recommend}</p>
-              <p>Analytical: ${journalistStats.analytical}</p>
-              <p>Wow: ${journalistStats.wow}</p>
-            `;
+        }
+    });
+    console.log(radarChart)
+    document.getElementById('stats').innerHTML += `
+          <p>Useful: ${journalistAverage.useful_avg}</p>
+          <p>Touched: ${journalistAverage.touched_avg}</p>
+          <p>Recommend: ${journalistAverage.recommend_avg}</p>
+          <p>Analytical: ${journalistAverage.analytical_avg}</p>
+          <p>Wow: ${journalistAverage.wow_avg}</p>
+        `;
     }
     );
 });
